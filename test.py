38526e51ca322323bae85fe43869fa50f8ccbccf
ZegesMenden <camkullberg@gmail.com>
2022-04-25 10:11:40 -0400

    registry test

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-import pytvc
-from pytvc.physics import *
+def makeReg():
+
+    registry = {}
+
+    def reg(func):
+        registry[func.__name__] = func
+        return func
+
+    reg.all = registry
+    return reg
+
+r = makeReg()
+
+@r
+def test1():
+    print("test1")
+
+@r
+def test2():
+    print("test2")
+
+print(r.all)
+for i in r.all:
+    r.all[i]()
