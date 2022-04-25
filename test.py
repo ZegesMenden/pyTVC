ab7339105bca2c096122940bd3039f8a3e20d27b
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2022-04-25 19:52:56 -0400

    bare minimum simulation

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 import pytvc
-from pytvc.physics import *
-from pytvc.simulation import rocket
+from pytvc.simulation import rocket, sim
+from pytvc.physics import Vec3, Quat, TVC, rocketMotor
+
+simulation: sim = sim()
+
+rocket_1: rocket = rocket(name="rocket_1", log_values=True)
+
+tvc_1: TVC = TVC(name="tvc_1")
+motor_1: rocketMotor = rocketMotor(filePath="C:/Users/Cameron/Documents/vscode/rockets/simulation/G-FIELD/motors/Estes_E12.rse", timeStep=1000)
+tvc_1.add_motor(motor_1)
+rocket_1.add_tvc_mount(tvc_mount=tvc_1, position=Vec3(0.2, 0.0, 0.0))
+simulation.add_rocket(rocket_1)
+
+simulation.run()
