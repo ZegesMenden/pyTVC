b9ecf0afca390c9c844d7e26206295241011fd96
Cam Kullberg <camkullberg@gmail.com>
2026-03-25 10:34:53 -0400

    Change description to match project

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     author="Cameron Kullberg",
     author_email="camkullberg@gmail.com",
-    description="Python package for generating 3d solder stencils from gerber files",
+    description="Python package for simulating amateur rockets and control systems",
     packages=["pytvc"],
     license="MIT",
     install_requires=[
